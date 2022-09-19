992ef6c29bfa15104e1e6d634d0a9e2bb7cacbd1
João Pedro Alcântara P. Calzá <112181440+Jotinha29@users.noreply.github.com>
2022-09-19 20:04:51 -0300

    Create AULA.04.js

---
+++ AULA.04.js
@@ -0,0 +1,5 @@
+var listaFilmes =["https://media.fstatic.com/KwH9KRVy9I8mOCFpEkoXAoFSDuM=/290x478/smart/media/movies/covers/2018/01/d0186f6d-d3ae-4a40-adb6-36381344540d.jpg", "https://br.web.img3.acsta.net/pictures/16/09/21/19/14/207893.jpg", "https://br.web.img3.acsta.net/pictures/21/02/05/15/12/2855710.jpg"];
+
+for (var i = 0; i < listaFilmes.length; i++){
+  document.write("<img src=" + listaFilmes[i]+">");
+}
