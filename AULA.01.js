ffd1d0f022454154290746734ad57e7d9f1e2215
João Pedro Alcântara P. Calzá <112181440+Jotinha29@users.noreply.github.com>
2022-09-19 19:31:56 -0300

    Código em JavaScript da aula 01 ImersaoDev.

---
+++ AULA.01.js
@@ -0,0 +1,16 @@
+var nome = "Jota"
+
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 7
+var notaTerceiroBimestre = 4
+var notaQuartoBimestre = 2
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) 
+var notaBoletim = notaFinal / 4
+
+var notaFixada = notaBoletim.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// comentário em JS
